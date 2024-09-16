3d4b4a0eff33d555d444414a98c04172a404e7be
Imeth Sasvin <80964121+ImethSasvin@users.noreply.github.com>
2024-09-16 20:08:59 +0530

    Added main files
    added the Main Files

---
+++ app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
@@ -0,0 +1,37 @@
+package com.example.bankaccountprogram
+
+import java.time.temporal.TemporalAmount
+
+class BankAccount (
+    var accountHolder: String,
+    var balance: Double,)
+
+{
+
+    private val transactionHistory = mutableListOf<String>()
+
+    fun deposit(amount: Double){
+
+        balance += amount
+        transactionHistory.add("$accountHolder deposited $$amount")
+
+    }
+
+    fun withdraw(amount: Double){
+
+        if(amount<balance){
+            balance -= amount
+            transactionHistory.add("$accountHolder withdrew $$amount")
+        }else{
+            println("You don't have enough funds to withdraw $$amount")
+        }
+    }
+
+    fun displayTransactionHistory(){
+
+        for(transaction in transactionHistory){
+            println(transaction)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/bankaccountprogram/BankMain.kt
@@ -0,0 +1,15 @@
+package com.example.bankaccountprogram
+
+fun main(){
+
+    val  imethsBankAcc = BankAccount("Imeth Sasvin", 2500.00)
+
+    imethsBankAcc.deposit(2500.00)
+    imethsBankAcc.withdraw(1502.00)
+    imethsBankAcc.deposit(2541.00)
+    imethsBankAcc.withdraw(3120.00)
+
+    imethsBankAcc.displayTransactionHistory()
+
+
+}
